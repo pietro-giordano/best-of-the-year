81b925712d3e1c1a874c09c2d96f214dfc414b96
Pietro Giordano <giopi87@gmail.com>
2023-06-20 14:33:11 +0200

    Initial commit

---
+++ src/test/java/org/lessons/java/bestoftheyear/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.bestoftheyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
